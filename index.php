b132c852a07af73926506972f0af08ecff977677
Emil Folino <emilfolino@gmail.com>
2018-12-21 10:29:25 +0100

    Initial commit with first view

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Me-app</title>
+
+    <link rel="stylesheet" href="style.min.css" />
+</head>
+<body>
+    <div class="container match">
+        <div class="match-selector">
+            <a class="single-link" href="single.php"><span class="aligner">Single</span></a>
+            <a class="double-link" href="double.php"><span class="aligner">Double</span></a>
+        </div>
+    </div>
+</body>
+</html>
